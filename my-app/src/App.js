904c7802d5167da45d0bd665393ce9541a62781e
Andrew <drewmalubag9@gmail.com>
2026-02-06 22:54:05 +0800

    First React Journey (Increase & Decrease)

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="container">
+      <h1>Count: {count}</h1>
+      <div className="buttons">
+        <button onClick={() => setCount(count + 1)}>+</button>
+        <button onClick={() => setCount(count - 1)}>-</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
