785d2bfa0dbfa0107ab86e71237f7dcc10d32992
Andrew <drewmalubag9@gmail.com>
2026-02-07 20:20:59 +0800

    My React Course

---
+++ react-course/src/Footer.jsx
@@ -0,0 +1,11 @@
+
+function Footer() {
+    return (
+        <footer>
+            <p>&copy; { new Date().getFullYear()} My Website Course Andrew B. Malubag</p>
+        </footer>
+    );        
+}
+
+
+export default Footer
