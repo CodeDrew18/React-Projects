3a08ae599ca38cde54876dfb860728df8c9dac8d
Andrew <drewmalubag9@gmail.com>
2026-02-10 12:32:34 +0800

    Styling for React

---
+++ react-components-css/src/App.jsx
@@ -0,0 +1,19 @@
+// How to Style React Components with CSS
+
+// (not including external frameworks or preprocessors)
+
+// 1. External
+// 2. Modules
+// 3. Inline
+
+import Button from './Button'
+
+function App() {
+  return (
+    <>
+      <Button />
+    </>
+  );
+}
+
+export default App
